e5f0d7513e9420223187e5b34ad6ef00241f1d6d
confident2 <mykel.anigboya@yahoo.com>
2024-03-13 07:16:26 -0700

    all tech not commit

---
+++ src/components/Public.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+const Public = () => {
+  const content = (
+    <section className="public">
+      <header>
+        <h1>
+          Welcome to <span className="nowrap">OASIS TASK BOOT</span>
+        </h1>
+      </header>
+      <main className="public__main">
+        <p>E-Commerce on steroid</p>
+        <address className="public__addr">
+          Dan D. Repairs
+          <br />
+          555 Foo Drive
+          <br />
+          Foo City, CA 12345
+          <br />
+          <a href="tel:+15555555555">(555) 555-5555</a>
+        </address>
+        <br />
+        <p>Owner: Michael Anigboya</p>
+      </main>
+      <footer>
+        <Link to="/login">Employee Login</Link>
+      </footer>
+    </section>
+  );
+  return content;
+};
+export default Public;
